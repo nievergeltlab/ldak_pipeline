456570d886bc4bed7d8206afeda6cccffffd707f
Adam X. Maihofer <amaihofer@ucsd.edu>
2018-02-02 11:16:20 -0800

    Add files via upload

--- 1_get_h2_v2.sh
+++ 1_get_h2_v2.sh
@@ -2,10 +2,25 @@
 
 
 #Set working directory 
-wd=/home/cnieverg/gctam/ldak_test
+wd=/mnt/sdb/genetics/delme/ldaktest
 
 #Call into it
  cd $wd
+ 
+#Are you working on LISA? Write yes if you are, no if you are not
+lisa=no
+
+
+#If not, make a dir for scratch files
+if [ $lisa != "yes" ]
+then
+ if [ ! -e "$wd"/scratch ]
+ then
+  mkdir "$wd"/scratch
+ fi
+ lisa="$wd"/scratch
+fi
+ 
 
 #Put location of LDAK binary here
  ldak="$wd"/ldak5.linux
@@ -16,14 +31,19 @@ wd=/home/cnieverg/gctam/ldak_test
  p_loc="$wd"/plink
  
 #Name of your study (all outputs will have this name)
- sname=eurtest
+ sname=eurtests
 
 #Set and make directories for outputs. Copy and paste this part, just leave as defaults. 
 
-
+ if [ ! -e "$wd"/errandout ]
+ then
+  mkdir "$wd"/errandout
+ fi
+ 
  #1: Location of merged genotypes . 
  outdir_mgeno="$wd"/merged_genotypes
  #2A1 and 2A2: Location of matrix for calculation of relatedness and pcs
+ outdir_relatednessa="$wd"/relatedness
  outdir_relatedness="$wd"/relatedness/bychr 
  #3A: Location of weight files
  outdir_weights="$wd"/weights
@@ -37,7 +57,7 @@ wd=/home/cnieverg/gctam/ldak_test
  then
   mkdir $outdir_mgeno
  fi
- 
+
  if [ ! -e $outdir_relatedness ]
  then
   mkdir -p $outdir_relatedness
@@ -81,8 +101,8 @@ wd=/home/cnieverg/gctam/ldak_test
 
 #Note: By default this will export only SNPs with ACGT alleles (As of writing, LDAK does not like alleles with more than 1 character)
 #If you don't want to do this, set -a to no. But to avoid errors I recommend just leaving this as is
- qsub -t1-22  -l walltime=00:15:00 scripts/1_process_data_merge.qsub -d $wd -e errandout/ -o errandout/ -F "-g $starting_geno_dir -p $p_loc -n $sname -o $outdir_mgeno -a yes -q $gtthresh -m $mafthresh" 
- 
+ qsub -t1-22  -l walltime=00:15:00 scripts/1_process_data_merge.qsub -d $wd -e errandout/ -o errandout/ -F "-g $starting_geno_dir -p $p_loc -n $sname -o $outdir_mgeno -a yes -q $gtthresh -m $mafthresh -x $lisa" 
+
 ###Step 2: Identify related subjects
 
 #We are doing this because SNP based heritability analysis requires related people to be removed
@@ -99,7 +119,7 @@ wd=/home/cnieverg/gctam/ldak_test
  useweights=no #Use weights in computation of GRM. Set to 'no' if you don't want to use weights. For this step, don't use weights.
 
 #If output folder doesnt exist yet, just make it
- qsub -t1-22 -l walltime=00:15:00 scripts/2a_identify_related.qsub -d $wd -e errandout/ -o errandout/ -F "-e $ldak -l ldakmodel -p $power -q $pruning -w $windowkb -r $r2 -n $sname -g $genodir_relatedness -u $useweights -o $outdir_relatedness "
+ qsub -t1-22 -l walltime=00:15:00 scripts/2a_identify_related.qsub -d $wd -e errandout/ -o errandout/ -F "-e $ldakf -l ldakmodel -p $power -q $pruning -w $windowkb -r $r2 -n $sname -g $genodir_relatedness -u $useweights -o $outdir_relatedness -x $lisa"
 
 ##2A2: Merge chromosomal kinship matrices, identify related subjects to remove. Calculate PCs to include in analysis (probably redundant due to GRM, but OK to include)
 
@@ -113,14 +133,14 @@ wd=/home/cnieverg/gctam/ldak_test
  keepfile=xxxx #keep flag for retaining custom subset of people. If you specify a keep file, subjects NOT in this file will be removed from analysis
  kinship_matrix_location=$outdir_relatedness #Output of kinship analysis (PCAs, grm, etc)
  outdir_kinship_grm="$wd"/relatedness
- qsub -l walltime=00:15:00 scripts/2a2_merge_related_grm.qsub -d $wd -e errandout/ -o errandout/ -F "-e $ldak -p $pheno_file -k $keepfile -r $rel_cutoff -g  $kinship_matrix_location -n $sname -o $outdir_kinship_grm" 
+ qsub -l walltime=00:15:00 scripts/2a2_merge_related_grm.qsub -d $wd -e errandout/ -o errandout/ -F "-e $ldakf -p $pheno_file -k $keepfile -r $rel_cutoff -g  $kinship_matrix_location -n $sname -o $outdir_kinship_grm -x $lisa" 
 
  
 ##2B: Prepare phenotype and covariates data.
  
  #Program parameter settings
  pca_file="$outdir_kinship_grm"/"$sname"_relatedness_pruned.vect  #PCA file from last analysis
- phenofile=dummy.pheno #Phenotpye file should be blankspace delimited. It should have columns 1 and 2 be FID and IID. It must have a header. Put phenotype and covariates in here
+ phenofile=pgc-ptsd-all_v1.txt #Phenotpye file should be blankspace delimited. It should have columns 1 and 2 be FID and IID. It must have a header. Put phenotype and covariates in here
  phenoname=ptsd #Name of phenotype in pheno file
  covar_names=xxxx #Write covariate names here, if any. Delimit using commas e.g. C1,C2,C3 
  npcs=5 #Number of PCs to include. If none are included, PCA file will just be used to determine unrelated people to include
@@ -137,9 +157,9 @@ wd=/home/cnieverg/gctam/ldak_test
 
 #Note: Manual suggests doing this by cohort to prevent genotyping errors from influencing weights. However,this is impractical because of our number of cohorts and their wildly different sizes, so I don't do it!
  
- weightings_keepflag="$outdir_relatedness"/"$sname"_relatedness_pruned.keep #Put a list of subjects if you want to calculate weights in just a subset of subjects, namely the pruned subject list
+ weightings_keepflag="$outdir_relatednessa"/"$sname"_relatedness_pruned.keep #Put a list of subjects if you want to calculate weights in just a subset of subjects, namely the pruned subject list
 
- qsub -t1-22 -l walltime=00:05:00 scripts/3_ldak_weights.qsub -d $wd -e errandout/ -o errandout/  -F "-e $ldak -g $outdir_mgeno -n $sname -k $weightings_keepflag -o $outdir_weights "
+ qsub -t22 -l walltime=00:30:00 scripts/3_ldak_weights.qsub -d $wd -e errandout/ -o errandout/  -F "-e $ldak -g $outdir_mgeno -n $sname -k $weightings_keepflag -o $outdir_weights -x $lisa"
 
 #Join weightings (Should be very quick,maybe not even needed to run job script)
  for chr in {1..22} ; do $ldak --join-weights  "$outdir_weights"/sections_$chr --bfile "$outdir_mgeno"/"$sname"_"$chr" ; done ; 
@@ -151,13 +171,13 @@ wd=/home/cnieverg/gctam/ldak_test
  ldakmodel=ldak #If value set to gcta, no weights are used and power is set to -1. If set to 'unweighted', no weights will be used (power can be set with power). Otherwise, weights will be used
  power=-0.25 #Set power for LDAK GRM calculation. Speed recommends -0.25. GCTA is -1.
 
- qsub -t1-22 -l walltime=00:05:00 scripts/3B_ldak_kinship_v2_alt.qsub -d $wd -e errandout/ -o errandout/ -F "-e $ldak -l $ldakmodel -p $power -n $sname -g $outdir_mgeno -o $kinship_dir -p $power -w $outdir_weights"
+ qsub -t1-22 -l walltime=00:05:00 scripts/3B_ldak_kinship_v2_alt.qsub -d $wd -e errandout/ -o errandout/ -F "-e $ldakf -l $ldakmodel -p $power -n $sname -g $outdir_mgeno -o $kinship_dir -p $power -w $outdir_weights -x $lisa"
 
 #3C: Merge GRMs into entire autosome GRM (inputs: a GRM list and output file name)
 
 #Program parameters:
  biascheck=yes #Set to yes if you want to calculate GRMs for 1/4 segments of the genome, to be used for estimating bias
- qsub -l walltime=00:05:00 scripts/3C_merge_kinships.qsub -d $wd -e errandout/ -o errandout/ -F "-e $ldak -k $kinship_dir -o $combkinship_dir -f kinships -b yes "
+ qsub -l walltime=00:05:00 scripts/3C_merge_kinships.qsub -d $wd -e errandout/ -o errandout/ -F "-e $ldak -k $kinship_dir -o $combkinship_dir -f kinships -b yes -x $lisa"
 
 ##4A: Heritablity estimates
 
@@ -168,13 +188,13 @@ wd=/home/cnieverg/gctam/ldak_test
  outname_append=sens #Extra output names to append
  prevalence=xxxx #Extra command to set prevalence, so h2 is on the right scale
  
- qsub -l walltime=00:05:00 scripts/4_h2.qsub -d $wd -e errandout/ -o errandout/ -F "-e $ldak  -p $phenotype -c $covar -g $combkinship_dir -k $keeplist -o $ldak_resultsdir -a $outname_append -b yes -r $prevalence"
+ qsub -l walltime=00:05:00 scripts/4_h2.qsub -d $wd -e errandout/ -o errandout/ -F "-e $ldak  -p $phenotype -c $covar -g $combkinship_dir -k $keeplist -o $ldak_resultsdir -a $outname_append -b yes -r $prevalence -x $lisa"
 
 #Once this is done we can estimate the bias due to cryptic relatedness:
  grep Her_K "$ldak_resultsdir"/"$outname_append"quad{A,B,C,D,ALL}.reml | awk '(NR<=4){sum+=$2}(NR>4){sum2+=$2}END{I=(sum-sum2)/3;print "Inflation:", I, "=", I/sum2*100,"%"}'
 
 #Now we can do the actual estimate of h2 snp
- qsub -l walltime=00:05:00 scripts/4_h2.qsub -d $wd -e errandout/ -o errandout/ -F "-e $ldakf  -p $phenotype -c $covar -g $combkinship_dir -k $keeplist -o $ldak_resultsdir -a $outname_append -b no -r $prevalence"
+ qsub -l walltime=00:05:00 scripts/4_h2.qsub -d $wd -e errandout/ -o errandout/ -F "-e $ldakf  -p $phenotype -c $covar -g $combkinship_dir -k $keeplist -o $ldak_resultsdir -a $outname_append -b no -r $prevalence -x $lisa"
  grep Her_K "$ldak_resultsdir"/"$outname_append"full.reml
 
   
